4b0f18b55906dc89022700defd46dca3bf9b3a61
FarmingFortressLOL <166163263+FarmingFortressLOL@users.noreply.github.com>
2024-04-05 14:07:19 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,28 @@
+mySprite: Sprite = None
+mySprite2: Sprite = None
+mySprite2.follow(mySprite)
+mySprite2 = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 2 2 . . . . . . . . . 
+            . . . . . 2 2 . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.enemy)
+mySprite = sprites.create(assets.image("""
+    redy
+"""), SpriteKind.player)
+scene.set_background_color(13)
+controller.move_sprite(mySprite, controller.dx(), controller.dy())
+scene.camera_follow_sprite(mySprite)
